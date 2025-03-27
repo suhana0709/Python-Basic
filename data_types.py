337ca9060b012166a88cbbe101f6c260526adfdb
suhana0709 <farjanajaman1975@gmail.com>
2025-03-27 09:12:07 -0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,10 @@
+
+#making the variables and chacking the data type of it
+a = 1
+print("Data-Type of a is: ", type(a))
+b = 2.5
+print("Data-Type of b is: ", type(b))
+c = "mango"
+print("Data-Type of c is: ", type(c))
+d = True
+print("Data-Type of d is: ", type(d))
