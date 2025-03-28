42444b69f06ef3f416d42e154f20238d48cd6b28
suhana0709 <farjanajaman1975@gmail.com>
2025-03-28 09:15:54 -0500

    Add files via upload

---
+++ average.py
@@ -0,0 +1,16 @@
+#storing the height of the trees in variables
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 96
+tree5 = 11
+#adding the values and storing in one variable 'sum'
+sum = (tree1 + tree2 + tree3 + tree4 + tree5)
+#dividing the number by 5
+average = (sum/5)
+#printing statements
+print("**********************************************")
+print("\nNumbers: ", tree1,tree2,tree3,tree4,tree5)
+print("Finding the Average:\n Average = Total Sum of the Numbers/Total Amount of the Numbers")
+print("Sum = ", sum)
+print("Average =", average)
