6f6dabe2689b91336e9da3428274a84247b19204
suhana0709 <farjanajaman1975@gmail.com>
2025-04-04 07:58:45 -0500

    Add files via upload

---
+++ not_equal_operator.py
@@ -0,0 +1,20 @@
+a=10
+b=3
+c=3
+print(a != b)
+print(b != c)
+
+a = 'apple'
+b ='bananna'
+if a != b:
+    print(a, ' and ', b, ' are different.' )
+
+a = 4
+b = 5
+if (a==6) != (b==5):
+    print("Hello")
+
+a= int(input("Enter a number: "))
+if a%2 != 0:
+    print(a, 'is not an even number.')
+ 
