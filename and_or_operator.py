a9a79354e32655c7450fa7db7e4d46774c303121
suhana0709 <farjanajaman1975@gmail.com>
2025-04-04 07:32:31 -0500

    Add files via upload

---
+++ and_or_operator.py
@@ -0,0 +1,20 @@
+a = 3
+b = 9
+c = 0
+if a and b and c:
+    print("All numbers have boolean value as TRUE.")
+else:
+    print("All numbers have boolean value as FALSE.")
+
+d = 10
+e = -10
+f = 0
+if d>0 or e>0:
+    print("Either of the numbers is greater than zero.")
+else:
+    print("None of the numbers is greater than zero.")
+
+if e>0 or f>0:
+    print("Either oof the numbers is greater than zero.")
+else:
+    print("Neither of the numbers is greater than than zero.")
